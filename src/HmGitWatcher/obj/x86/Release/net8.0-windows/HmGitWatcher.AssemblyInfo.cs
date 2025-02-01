464ca6eeb4f64f8925a6f3acf6d2ffc89a0f6c8f
komiyamma <komiyama0629@gmail.com>
2025-02-01 17:21:41 +0900

    ...

--- src/HmGitWatcher/obj/x86/Release/net8.0-windows/HmGitWatcher.AssemblyInfo.cs
+++ src/HmGitWatcher/obj/x86/Release/net8.0-windows/HmGitWatcher.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("2024-2025 @ Akitsugu Komiyama")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0.0+ee56703f0d69fe060ca8f77b3a196871f5d34561")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0.0+18d937f05a2c620ef82a3d78ae889b0e43547dc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("HmGitWatcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HmGitWatcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
