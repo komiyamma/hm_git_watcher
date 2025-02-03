8b1676db9f123d070eb6dae534a1ef43f6b61aaf
komiyamma <komiyama0629@gmail.com>
2025-02-03 18:01:26 +0900

    AsyncPopupMenuの実装

--- src/HmGitWatcher/AsyncPopupMenu.cs
+++ src/HmGitWatcher/AsyncPopupMenu.cs
@@ -9,26 +9,50 @@ namespace HmGitWatcher;
 using System;
 using System.Drawing;
 using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 public partial class HmGitWatcher
 {
     static PopupMenuAsync popup_menu;
 
-    public void ShowAsyncPopupMenu(dynamic func)
+    public void ShowAsyncPopupMenu(dynamic func, string menu01, string menu02 = null, string menu03 = null, string menu04 = null, string menu05 = null, string menu06 = null, string menu07 = null, string menu08 = null, string menu09 = null)
+
     {
-        if (popup_menu != null)
+        try
         {
-            popup_menu.Dispose();
-            popup_menu = null;
+            // menu01～ menu09 までの引数を配列に格納
+            string[] menuArray = { menu01, menu02, menu03, menu04, menu05, menu06, menu07, menu08, menu09 };
+            // nullなものはカット
+            if (menuArray == null)
+            {
+                Hm.OutputPane.Output("メニューへの引数がnullです。\r\n");
+                return;
+            }
+
+            if (menuArray.Length == 0)
+            {
+                Hm.OutputPane.Output("メニューへの引数が配列が空です。\r\n");
+            }
+
+            if (popup_menu != null)
+            {
+                popup_menu.Dispose();
+
+                popup_menu = null;
+            }
+
+            popup_menu = new PopupMenuAsync(func, menuArray);
+
+            // 例として、特定の場所にポップアップメニューを表示
+            var x = Hm.Edit.MousePos.X;
+            var y = Hm.Edit.MousePos.Y;
+
+            popup_menu.ShowPopupMenu(new Point(x, y));
         }
-
-
-        popup_menu = new PopupMenuAsync();
-
-        // 例として、特定の場所にポップアップメニューを表示
-        var x = Hm.Edit.MousePos.X;
-        var y = Hm.Edit.MousePos.Y;
-        popup_menu.ShowPopupMenu(new Point(x, y));
+        catch (Exception ex)
+        {
+            Hm.OutputPane.Output(ex.Message + "\r\n");
+        }
     }
 }
 
@@ -36,36 +60,73 @@ internal class PopupMenuAsync
 {
     private ContextMenuStrip contextMenu;
 
-    public PopupMenuAsync()
+    private string[] menuArray;
+
+    private dynamic jsCallBackFunc;
+
+    public PopupMenuAsync(dynamic func, string[] menu_array)
     {
+        if (func == null)
+        {
+            return;
+        }
+
+        if (menu_array == null)
+        {
+            return;
+        }
+
+        this.jsCallBackFunc = func;
+
+        this.menuArray = menu_array;
+
         // コンテキストメニューの新規作成
         contextMenu = new ContextMenuStrip();
 
-        // メニュー項目の追加
-        ToolStripMenuItem item1 = new ToolStripMenuItem("Item 1");
-        item1.Click += (sender, e) => {
-            Console.WriteLine("Item 1 clicked");
-            // ここにItem 1がクリックされた時の処理を記述
-        };
-        contextMenu.Items.Add(item1);
+        foreach (var menuString in menuArray)
+        {
+            if (menuString == null)
+            {
+                continue;
+            }
+            if (menuString == "---")
+            {
+                contextMenu.Items.Add(new ToolStripSeparator());
+                continue;
+            }
 
-        ToolStripMenuItem item2 = new ToolStripMenuItem("Item 2");
-        item2.Click += (sender, e) => {
-            Console.WriteLine("Item 2 clicked");
-            // ここにItem 2がクリックされた時の処理を記述
-        };
-        contextMenu.Items.Add(item2);
+            ToolStripMenuItem item = new ToolStripMenuItem(menuString);
+            item.Click += On_PopupMenuItem_Click;
 
-        // 区切り線の追加
-        contextMenu.Items.Add(new ToolStripSeparator());
+            contextMenu.Items.Add(item);
+        }
+    }
 
-        ToolStripMenuItem exitItem = new ToolStripMenuItem("Exit");
-        exitItem.Click += (sender, e) => {
-            // アプリケーション終了の処理（フォーム不要）
-            Console.WriteLine("Exit selected, but console will not exit automatically.");
-            // 必要であれば、ここでアプリケーションの終了に関する処理を記述
-        };
-        contextMenu.Items.Add(exitItem);
+    private void On_PopupMenuItem_Click(object sender, EventArgs e)
+    {
+        // senderをToolStripMenuItem型にキャスト
+        ToolStripMenuItem clickedItem = (ToolStripMenuItem)sender;
+
+        // 親のContextMenuStripを取得
+        ContextMenuStrip parentMenu = (ContextMenuStrip)clickedItem.Owner;
+
+        // メニュー項目のコレクションから、クリックされた要素のインデックスを取得
+        int index = parentMenu.Items.IndexOf(clickedItem);
+
+        try
+        {
+            jsCallBackFunc(index, clickedItem?.Text ?? "");
+        }
+        catch (Exception ex)
+        {
+            // Hm.OutputPane.Output(ex.Message + "\r\n");
+        }
+        finally
+        {
+            contextMenu.Text = ""; // この無意味な記述によりGUIスレッドを使う。これにより最適化してもjsCallBackFuncの終わりを待つことが保証されるという点で意味がある。
+        }
+        // ここでインデックスを使った処理を行う
+        // Hm.OutputPane.Output($"「{clickedItem.Text}」がクリックされました。インデックス: {index}\r\n");
     }
 
     // ポップアップメニューを表示するメソッド
