2f98c3d66114bf1dbf0d2f7037fbc52b8915d9f6
komiyamma <komiyama0629@gmail.com>
2025-02-07 07:05:21 +0900

    不要なアルゴリズムを簡素化。

--- HmGitWatcherMacroReExecute.js
+++ HmGitWatcherMacroReExecute.js
@@ -20,43 +20,29 @@ function shouldMacroReExecute() {
 
     var eventId = event();
 
-    // 「手動実行」か「ファイルを開いた直後」もしくは「ファイルを保存時」の場合に処理を継続
+    // 「手動実行」か「ファイルを開いた直後」もしくは「ファイルを保存時」の場合にのみ処理を継続
     if (eventId != 0 && eventId != 1 && eventId != 3) {
         return 0;
     }
 
-    var currentFileFullPath = filename2() || "";
-
-    var lastFileStaticVariableLabel = "LastFile:" + hidemaru.getJsMode(); // まぁ適当にそれぞれの
-
-    // 「ファイルを保存時」の自動起動マクロから呼ばれている際は、
-    // 前回チェック時と同じファイル名なら何もしない。
-    if (eventId == 3) {
-
-        // 保存直前からの呼び出しは対象外。保存直後｛ event==3 && geteventparam(0)==1 ｝から呼び出すこと
-        // このタイミングで以後の処理をしたとしても、直後にファイルに紐づいたjsmode(の実行空間)が解放されるので無意味なため。
-        if (geteventparam(0) == 0) {
-            return 0;
-        }
-
-        // コンポーネントが解放されてしまっているなら、jsmodeの実行空間がクリアされてしまっている。改めて実行する必要がある。
-        if (typeof (gitWatcherComponent) == "undefined") {
-            setstaticvariable(lastFileStaticVariableLabel, currentFileFullPath, 2); // 最後の解析ファイルフルパスを保持(不要なHmGitWatcher駆動を避ける)
-
-            return currentFileFullPath ? 1 : 0;
-        }
-
-        // 保存直後、かつ、コンポーネントの残っている状態で、かつ、ファイル名が前回と同じであるならば、改めてマクロの続きを実行する必要はない。切り上げる。
-        if (getstaticvariable(lastFileStaticVariableLabel, 2) == currentFileFullPath) {
-            // 前回HmGitWatcher開始時と同じファイルなのでパス
-            return 0;
-        }
+    // 保存直前からの呼び出しは対象外。保存直後｛ event==3 && geteventparam(0)==1 ｝からの呼び出しには対応。
+    // 「ファイル名をつけて保存」だと、保存直前のタイミングで処理をしたとしても、直後にファイルに紐づいたjsmode(の実行空間)が解放されるので無意味なため。
+    if (eventId == 3 && geteventparam(0) == 0) {
+        return 0;
     }
 
-    setstaticvariable(lastFileStaticVariableLabel, currentFileFullPath, 2); // 最後の解析ファイルフルパスを保持(不要なHmGitWatcher駆動を避ける)
+    // ファイル名が無題。何もしない。
+    if (!filename2()) {
+        return 0;
+    }
 
-    // 無題スタートは対象外
-    return currentFileFullPath ? 1 : 0;
+    // コンポーネントが解放されてしまっているなら、jsmodeの実行空間がクリアされてしまっている。改めて実行する必要がある。
+    if (typeof (gitWatcherComponent) == "undefined") {
+        return 1;
+    }
+
+    // コンポーネントは存在している。何もしない。
+    return 0;
 }
 
 setVar("#ShouldMacroReExecute", shouldMacroReExecute());
