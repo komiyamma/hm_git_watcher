037ab956169f97284ede4817c3ddef330f52ea22
komiyamma <komiyama0629@gmail.com>
2025-04-16 23:29:48 +0900

    クラス分け

--- src/4.8/HmGitWatcherFW/GitCommitDryRun.cs
+++ src/4.8/HmGitWatcherFW/GitCommitDryRun.cs
@@ -3,83 +3,82 @@ using System;
 using System.Diagnostics;
 using System.Text;
 
-namespace HmGitWatcher
+namespace HmGitWatcher;
+
+public partial class HmGitWatcher
 {
-    public partial class HmGitWatcher
+    public string GetGitCommitDrayRun(string workingDirectory)
     {
-        public string GetGitCommitDrayRun(string workingDirectory)
+        ProcessStartInfo startInfo = new ProcessStartInfo
         {
-            ProcessStartInfo startInfo = new ProcessStartInfo
-            {
-                FileName = "git",
-                Arguments = "commit --dry-run -z",
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                WorkingDirectory = workingDirectory,
-                UseShellExecute = false, // ShellExecute を false にしてリダイレクトを有効にする
-                CreateNoWindow = true,  // コンソールウィンドウを表示しない
-                StandardOutputEncoding = Encoding.UTF8, // 出力エンコーディングをUTF-8に指定
-                StandardErrorEncoding = Encoding.UTF8
-            };
+            FileName = "git",
+            Arguments = "commit --dry-run -z",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            WorkingDirectory = workingDirectory,
+            UseShellExecute = false, // ShellExecute を false にしてリダイレクトを有効にする
+            CreateNoWindow = true,  // コンソールウィンドウを表示しない
+            StandardOutputEncoding = Encoding.UTF8, // 出力エンコーディングをUTF-8に指定
+            StandardErrorEncoding = Encoding.UTF8
+        };
 
-            try
-            {
-                string stdOutSum = "";
-                string stdErrSum = "";
-                Hm.OutputPane.Output(startInfo.Arguments + "\r\n");
+        try
+        {
+            string stdOutSum = "";
+            string stdErrSum = "";
+            Hm.OutputPane.Output(startInfo.Arguments + "\r\n");
 
-                using (Process process = new Process())
+            using (Process process = new Process())
+            {
+
+                process.StartInfo = startInfo;
+
+                process.OutputDataReceived += (sender, args) =>
                 {
-
-                    process.StartInfo = startInfo;
-
-                    process.OutputDataReceived += (sender, args) =>
+                    if (!string.IsNullOrEmpty(args.Data))
                     {
-                        if (!string.IsNullOrEmpty(args.Data))
+                        // ナル文字('\0')で分割
+                        string[] parts = args.Data.Split('\0');
+                        foreach (var part in parts)
                         {
-                            // ナル文字('\0')で分割
-                            string[] parts = args.Data.Split('\0');
-                            foreach (var part in parts)
+                            if (part.Length > 0)
                             {
-                                if (part.Length > 0)
-                                {
-                                    stdOutSum += part + "\r\n";
-                                }
+                                stdOutSum += part + "\r\n";
                             }
                         }
-                    };
+                    }
+                };
 
-                    process.ErrorDataReceived += (sender, args) =>
+                process.ErrorDataReceived += (sender, args) =>
+                {
+                    if (!string.IsNullOrEmpty(args.Data))
                     {
-                        if (!string.IsNullOrEmpty(args.Data))
+                        // ナル文字('\0')で分割
+                        string[] parts = args.Data.Split('\0');
+                        foreach (var part in parts)
                         {
-                            // ナル文字('\0')で分割
-                            string[] parts = args.Data.Split('\0');
-                            foreach (var part in parts)
+                            if (part.Length > 0)
                             {
-                                if (part.Length > 0)
-                                {
-                                    stdErrSum += part + "\r\n";
-                                }
+                                stdErrSum += part + "\r\n";
                             }
                         }
-                    };
+                    }
+                };
 
-                    process.Start();
-                    process.BeginOutputReadLine();
-                    process.BeginErrorReadLine();
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
 
-                    process.WaitForExit();
+                process.WaitForExit();
 
-                    return stdOutSum;
-                }
-            }
-            catch (Exception ex)
-            {
-                Hm.OutputPane.Output(startInfo.FileName + " " + startInfo.Arguments + $"実行中にエラーが発生しました: {ex} + \r\n");
-                return null; // エラー発生時もnullを返す
+                return stdOutSum;
             }
         }
-
+        catch (Exception ex)
+        {
+            Hm.OutputPane.Output(startInfo.FileName + " " + startInfo.Arguments + $"実行中にエラーが発生しました: {ex} + \r\n");
+            return null; // エラー発生時もnullを返す
+        }
     }
+
 }
---
+++ src/8.0/HmGitWatcher/FrontWindow.cs
@@ -0,0 +1,86 @@
+﻿using HmNetCOM;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HmGitWatcher;
+
+public partial class HmGitWatcher
+{
+    // GetParentWinndow
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetParent(IntPtr hWnd);
+
+    // FindWindow
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern IntPtr FindWindow(string lpClassName, IntPtr none);
+
+    // FindWindowExW の定義 (P/Invoke)
+    [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    public static extern IntPtr FindWindowEx(IntPtr hWndParent, IntPtr hWndChildAfter, string lpszClass, IntPtr none);
+
+    // ウィンドウハンドルのクラス名を取得
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount = 256);
+
+
+    // Hm.WindowHandleのクラス名。ストア版とデスクトップ版で異なため、それを考慮してキャッシュする。
+    string curHmWndClassNameCache = null;
+
+    private bool IsCurrentWindowFront()
+    {
+        IntPtr curHWnd = Hm.WindowHandle;
+
+        IntPtr parentWnd = GetParent(curHWnd);
+        // タブモードである
+        if (parentWnd != IntPtr.Zero)
+        {
+            int currentWindowBackGround = Hm.Edit.InputStates & 0x00000800;
+            // 非アクティブではない(=自分のプロセスはアクティブである)
+            if (currentWindowBackGround == 0)
+            {
+                return true;
+            }
+        }
+        // 非タブモードなら
+        else
+        {
+            // 自身のウィンドウハンドルのクラス名のキャッシュがない
+            if (curHmWndClassNameCache == null)
+            {
+                StringBuilder className = new StringBuilder(256);
+                GetClassName(curHWnd, className, className.Capacity);
+                // Hm.OutputPane.Output("className:" + className.ToString() + "\r\n");
+                curHmWndClassNameCache = className.ToString();
+            }
+
+            if (curHmWndClassNameCache?.Length > 0)
+            {
+                // そのクラス名で検索。（非タブモードならデスクトップ直下のルートウィンドウとして秀丸ウィンドが存在する)
+                IntPtr firstFindWnd = FindWindow(curHmWndClassNameCache, IntPtr.Zero);
+                // 自分が秀丸の中でトップのウィンドウである
+                if (firstFindWnd == curHWnd)
+                {
+                    // Hm.OutputPane.Output("トップウィンドウだよ\r\n");
+                    return true; ;
+                }
+                else
+                {
+                    IntPtr secondFindWnd = FindWindowEx(IntPtr.Zero, firstFindWnd, curHmWndClassNameCache, IntPtr.Zero);
+                    if (secondFindWnd == curHWnd)
+                    {
+                        // Hm.OutputPane.Output("セカンドウィンドウだよ\r\n");
+                        return true; ;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+}
